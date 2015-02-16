d97f67651b7f571d6f8d94093080af35a0d153f5
David Wicks <wicks.db@gmail.com>
2015-02-15 20:40:54 -0500

    Simplify vanilla playground, too.

--- playground-vanilla/playground.js
+++ playground-vanilla/playground.js
@@ -1,42 +1,48 @@
 // Get references to the DOM elements we care about.
-var result = document.querySelector(".result"),
+var output = document.querySelector(".output"),
     input = document.querySelector(".input"),
-    userOutput = document.querySelector(".userOutput"),
-    errorOutput = document.querySelector(".error"),
     errorMessageTimeout;
 
 // write(text) Writes text to user output.
-var write = function(text) {
-  userOutput.textContent += text;
+var write = function(text, messageType) {
+  var messageType = messageType || "",
+      p = document.createElement("p");
+  p.className = messageType;
+  p.textContent = text;
+
+  output.appendChild(p);
 }
 
 // clearOutput() Clears all text from user output.
 var clearOutput = function() {
-  userOutput.textContent = "";
+  output.textContent = "";
 }
 
 // evaluateUserCode() Evaluates the user script and show error messages.
 var evaluateUserCode = function() {
+  clearOutput();
+  // Get the input code from the textarea.
+  var code = input.value;
+
   if (errorMessageTimeout) {
     // If we were going to show an error message from previous input,
     // cancel that error message since the user is writing something new.
     clearTimeout(errorMessageTimeout);
-    errorOutput.textContent = "";
-    result.textContent = "";
   }
 
   try {
-    // Clear the user output.
-    clearOutput();
-    // Run the input code and put any text results in the result field.
-    result.textContent = eval(input.value);
+    // Run the input code and put any text outputs in the output field.
+    var result = eval(code);
+    if (result !== "") {
+      write(result, "result");
+    }
   }
   catch(err) {
     // displayError() will show the current error message on screen.
     var displayError = function() {
-      errorOutput.textContent = "Code error: " + err.message;
+      write(err.message, "error");
     };
-    // Call displayError() after 250ms.
+    // Call displayError() after 500ms.
     // The delay allows the user to keep typing without seeing a long series of error messages.
     errorMessageTimeout = setTimeout(displayError, 500);
   }
