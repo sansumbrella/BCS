d4509aace13d3a7f073e7e395840a5f45c30d137
David Wicks <wicks.db@gmail.com>
2015-02-15 20:27:41 -0500

    Simplifying code result output.

--- playground-codemirror/playground.js
+++ playground-codemirror/playground.js
@@ -1,8 +1,6 @@
 // Get references to the DOM elements we care about.
-var result = document.querySelector(".result"),
+var output = document.querySelector(".output"),
     input = document.querySelector(".input"),
-    userOutput = document.querySelector(".userOutput"),
-    errorOutput = document.querySelector(".error"),
     codeMirror = CodeMirror(input, {mode: "javascript",
                                     theme:'blackboard',
                                     lineNumbers: true,
@@ -10,40 +8,45 @@ var result = document.querySelector(".result"),
     errorMessageTimeout;
 
 // write(text) Writes text to user output.
-var write = function(text) {
-  userOutput.textContent += text;
+var write = function(text, messageType) {
+  var messageType = messageType || "",
+      p = document.createElement("p");
+  p.className = messageType;
+  p.textContent = text;
+
+  output.appendChild(p);
 }
 
 // clearOutput() Clears all text from user output.
 var clearOutput = function() {
-  userOutput.textContent = "";
+  output.textContent = "";
 }
 
 // evaluateUserCode() Evaluates the user script and show error messages.
 var evaluateUserCode = function(cm) {
+  clearOutput();
+  // Get the input code from CodeMirror.
   var code = cm.getValue();
-  console.log("yolo");
+
   if (errorMessageTimeout) {
     // If we were going to show an error message from previous input,
     // cancel that error message since the user is writing something new.
     clearTimeout(errorMessageTimeout);
-    errorOutput.textContent = "";
-    result.textContent = "";
   }
 
   try {
-    // Clear the user output.
-    clearOutput();
-    // Run the input code and put any text results in the result field.
-    var r = eval(code);
-    result.textContent = eval(code);
+    // Run the input code and put any text outputs in the output field.
+    var result = eval(code);
+    if (result !== "") {
+      write(result, "result");
+    }
   }
   catch(err) {
     // displayError() will show the current error message on screen.
     var displayError = function() {
-      errorOutput.textContent = "[error]: " + err.message;
+      write(err.message, "error");
     };
-    // Call displayError() after 250ms.
+    // Call displayError() after 500ms.
     // The delay allows the user to keep typing without seeing a long series of error messages.
     errorMessageTimeout = setTimeout(displayError, 500);
   }
